fb6d42f26b47e3e9abccc94db83200c0f10bd87a
sunjeong512 <select@snu.ac.kr>
2023-05-24 20:33:13 +0900

    Test: Commit 기능 확인
    - commit이 내용을 잘 반영하는지 확인하고자
    - 테스트로 commit 해봅니다.

--- src/pages/detail.js
+++ src/pages/detail.js
@@ -1,7 +1,28 @@
+import React, { useRef } from 'react';
+
 export default function Detail() {
-    return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <h1 className="text-6xl font-bold text-center text-tertiary">Detail</h1>
-        </main>
-    )
+  const fileInputRef = useRef(null);
+
+  const handleFileUpload = (e) => {
+    const file = e.target.files[0];
+    // 선택한 파일을 업로드하거나 처리하는 로직을 추가하세요
+    console.log('Uploaded file:', file);
+  };
+
+  return (
+    <div>
+      <input
+        type="file"
+        ref={fileInputRef}
+        style={{ display: 'none' }}
+        onChange={handleFileUpload}
+      />
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4"
+        onClick={() => fileInputRef.current.click()}
+      >
+        PDF 파일 선택
+      </button>
+    </div>
+  );
 }
