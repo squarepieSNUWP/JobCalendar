88fc2299733b7ca27b92f9e181f58b6631feecaa
Kim Nam Hee <103100783+kimnamheeee@users.noreply.github.com>
2023-05-27 01:13:05 +0900

    feat: 로그인 기능 추가

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,17 @@
-import '@/styles/globals.css'
-import Head from 'next/head'
+import "@/styles/globals.css";
+import { SessionProvider } from "next-auth/react";
+import Head from "next/head";
 
-export default function App({ Component, pageProps }) {
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
   return (
-    <>
+    <SessionProvider session={pageProps.session}>
       <Head>
         <title>🔥취뽀달력🔥-</title>
       </Head>
       <Component {...pageProps} />
-    </>
-  )
+    </SessionProvider>
+  );
 }
---
+++ src/pages/login.js
@@ -0,0 +1,5 @@
+import SignIn from "./api/auth/signin";
+
+export default function LoginPage() {
+  return <SignIn />;
+}
