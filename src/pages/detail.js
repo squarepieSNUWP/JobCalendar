4fa0ac1f985daf967e4596f9bb513fadc3912dad
sunjeong512 <select@snu.ac.kr>
2023-05-26 13:46:57 +0900

    Design: pdf 파일 삽입 부분 CSS
    - pdf 삽입 부분 CSS를 다듬었습니다.
    - 페이지 너비를 줄일 예정이어서
    - 기존보다 좁은 너비로 작업했습니다.

--- src/pages/detail.js
+++ src/pages/detail.js
@@ -19,45 +19,55 @@ export default function Detail() {
           style={{ display: 'none' }}
           onChange={handleFileUpload}
         />
-        <div class="inset-0 flex flex-col justify-center">
-          <div class="bg-gray-100 p-3 rounded-3xl mb-4 w-24 text-center border border-gray-300">
-            <p class="text-gray-800 font-bold">회사명</p>
+        <div class="inset-0 flex flex-col justify-end">
+          <div class="bg-primary p-0 rounded-full w-8 h-8 text-center">
+            <p class="text-white font-bold place-self-center pt-1">X</p>
           </div>
 
-          <div class="bg-white p-4 rounded-2xl shadow drop-shadow-md mb-9 w-full h-52">
+          <div class="place-self-center bg-white p-4 rounded-2xl shadow drop-shadow-md mb-9 w-3/4 h-52">
             </div>
+          
+          {/* 여기부터 pdf 파일 삽입 구간. */}
 
-          <div class="bg-white p-4 rounded-2xl shadow drop-shadow-lg w-full">
+          <div class="bg-secondary/80 p-3 py-5 place-self-center rounded-2xl shadow drop-shadow-lg w-3/4">
             <div className="flex flex-col">
-              <div className="flex flex-col mb-4">
-                <span>이력서</span>
-                <button
-                  className="bg-secondary hover:bg-primary text-white font-bold py-2 px-4 rounded-2xl mt-4"
-                  onClick={() => fileInputRef.current.click()}
-                >
-                  PDF 파일 선택
-                </button>
+
+              <div className="bg-white p-4 rounded-2xl">
+                <div className="flex flex-col mb-1">
+                  <span className="font-normal ml-2">이력서</span>
+                  <button
+                    className="bg-secondary/20 hover:bg-secondary/50 text-secondary py-2 px-4 rounded-2xl mt-2 "
+                    onClick={() => fileInputRef.current.click()}
+                  >
+                    PDF 파일 선택
+                  </button>
+                </div>
               </div>
 
-              <div className="flex flex-col mb-4">
-                <span>자소서</span>
-                <button
-                  className="bg-secondary hover:bg-primary text-white font-bold py-2 px-4 rounded-2xl mt-4"
-                  onClick={() => fileInputRef.current.click()}
-                >
-                  PDF 파일 선택
-                </button>
+              <div className="bg-white p-4 rounded-2xl mt-3">
+                <div className="flex flex-col mb-1">
+                  <span className="font-normal ml-2">자소서</span>
+                  <button
+                    className="bg-secondary/20 hover:bg-secondary/50 text-secondary py-2 px-4 rounded-2xl mt-2 "
+                    onClick={() => fileInputRef.current.click()}
+                  >
+                    PDF 파일 선택
+                  </button>
+                </div>
               </div>
 
-              <div className="flex flex-col mb-4">
-                <span>포트폴리오</span>
-                <button
-                  className="bg-secondary hover:bg-primary text-white font-bold py-2 px-4 rounded-2xl mt-4"
-                  onClick={() => fileInputRef.current.click()}
-                >
-                  PDF 파일 선택
-                </button>
+              <div className="bg-white p-4 rounded-2xl mt-3 ">
+                <div className="flex flex-col mb-1">
+                  <span className="font-normal ml-2">포트폴리오</span>
+                  <button
+                    className="bg-secondary/20 hover:bg-secondary/50 text-secondary py-2 px-4 rounded-2xl mt-2 "
+                    onClick={() => fileInputRef.current.click()}
+                  >
+                    PDF 파일 선택
+                  </button>
+                </div>
               </div>
+
           </div>
         </div>    
         </div>
