532e117ef19099802540724861db4b35ee829e6d
Kim Nam Hee <103100783+kimnamheeee@users.noreply.github.com>
2023-05-27 11:54:23 +0900

    style: 헤더 스타일 변경
    로그인 하기 전에만 'login' 버튼이 보이고, 로그인 된 상태의 유저에게는 'logout' 버튼이 보이도록 했습니다.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,31 +1,59 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useSession, signOut } from "next-auth/react";
 
 export default function Header() {
-    const route = useRouter();
+  const route = useRouter();
+  const { data: session } = useSession();
 
-    return (
-        <header className="flex items-center justify-between h-24 px-12 py-5 bg-white shadow-sm">
-            <div className="flex items-center">
-                <div className="mr-6">
-                    <Link href="/" className="text-2xl font-bold text-primary">
-                        🔥 취뽀달력 🔥
-                    </Link>
-                </div>
-                <nav className="text-lg">
-                    <Link href="/jobs" className={route.pathname === "/jobs" ? "mr-4 text-gray-700 hover:text-gray-900 font-black underline underline-offset-4" : "mr-4 text-gray-700 hover:text-gray-900"}>
-                        Jobs
-                    </Link>
-                    <Link href="/" className={route.pathname === "/" ? "mr-4 text-gray-700 hover:text-gray-900 font-black underline underline-offset-4" : "mr-4 text-gray-700 hover:text-gray-900"}>
-                        Calendar
-                    </Link>
-                </nav>
+  return (
+    <header className="flex items-center justify-between h-24 px-12 py-5 bg-white shadow-sm">
+      <div className="flex items-center">
+        <div className="mr-6">
+          <Link href="/" className="text-2xl font-bold text-primary">
+            🔥 취뽀달력 🔥
+          </Link>
+        </div>
+        <nav className="text-lg">
+          <Link
+            href="/jobs"
+            className={
+              route.pathname === "/jobs"
+                ? "mr-4 text-gray-700 hover:text-gray-900 font-black underline underline-offset-4"
+                : "mr-4 text-gray-700 hover:text-gray-900"
+            }
+          >
+            Jobs
+          </Link>
+          <Link
+            href="/"
+            className={
+              route.pathname === "/"
+                ? "mr-4 text-gray-700 hover:text-gray-900 font-black underline underline-offset-4"
+                : "mr-4 text-gray-700 hover:text-gray-900"
+            }
+          >
+            Calendar
+          </Link>
+        </nav>
+      </div>
+      <div>
+        {session ? (
+          <div className="flex">
+            <div className="mr-6">{session.user.name}님 환영합니다 !</div>
+            <div className="cursor-pointer" onClick={signOut}>
+              Log out
             </div>
-            <div>
-                <Link href="/login" className="mr-4 text-gray-700 hover:text-gray-900">
-                    Login
-                </Link>
-            </div>
-        </header>
-    )
+          </div>
+        ) : (
+          <Link
+            href="/login"
+            className="mr-4 text-gray-700 hover:text-gray-900"
+          >
+            Login
+          </Link>
+        )}
+      </div>
+    </header>
+  );
 }
