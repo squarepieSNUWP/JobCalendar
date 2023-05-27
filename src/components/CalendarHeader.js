0d2b173ebd5c361fdbc24dd9b9d0c4573f597c71
MyeongwonKim <mw7539@naver.com>
2023-05-28 01:56:16 +0900

    Feat: 이전달 / 오늘 / 다음달 버튼 기능 추가

--- src/components/CalendarHeader.js
+++ src/components/CalendarHeader.js
@@ -1,4 +1,29 @@
 export default function CalendarHeader({ months, today, selectedYear, selectedMonth, setSelectedYear, setSelectedMonth }) {
+
+  function goPrev() {
+    if (selectedMonth == 1) {
+      setSelectedYear(selectedYear - 1);
+      setSelectedMonth(12);
+      return;
+    }
+    setSelectedMonth(selectedMonth - 1);
+  }
+
+  function goNext() {
+    if (selectedMonth == 12) {
+      setSelectedYear(selectedYear + 1);
+      setSelectedMonth(1);
+      return;
+    }
+    setSelectedMonth(selectedMonth + 1);
+  }
+
+  function goToday() {
+    setSelectedYear(today.year);
+    setSelectedMonth(today.month);
+  }
+
+
   return (
     <div className="w-full flex justify-between mb-4">
       <div className="flex items-baseline">
@@ -7,9 +32,12 @@ export default function CalendarHeader({ months, today, selectedYear, selectedMo
       </div>
 
       <div className="flex justify-end items-end">
-        <button className="px-2 py-1 text-sm font-normal border border-gray-300 rounded bg-transparent hover:bg-blue-500 hover:opacity-70 transition duration-1000 ease-in-out">&lt;</button>
-        <button className="px-2 py-1 text-sm font-normal border border-gray-300 rounded bg-transparent hover:bg-blue-500 hover:opacity-70 transition duration-1000 ease-in-out">Today</button>
-        <button className="px-2 py-1 text-sm font-normal border border-gray-300 rounded bg-transparent hover:bg-blue-500 hover:opacity-70 transition duration-1000 ease-in-out">&gt;</button>
+        <button
+          className="px-2 py-1 text-sm font-normal border border-gray-300 rounded bg-transparent hover:bg-blue-500 hover:opacity-70 transition duration-1000 ease-in-out" onClick={goPrev}>&lt;</button>
+        <button
+          className="px-2 py-1 text-sm font-normal border border-gray-300 rounded bg-transparent hover:bg-blue-500 hover:opacity-70 transition duration-1000 ease-in-out" onClick={goToday}>Today</button>
+        <button
+          className="px-2 py-1 text-sm font-normal border border-gray-300 rounded bg-transparent hover:bg-blue-500 hover:opacity-70 transition duration-1000 ease-in-out" onClick={goNext}>&gt;</button>
       </div>
     </div>
   );
