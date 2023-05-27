55ac3e2953259d99f51e86cb911c3d025222cc3d
sunjeong512 <select@snu.ac.kr>
2023-05-27 18:41:19 +0900

    Feat: PDF 파일 embed 기능 수정
    - 파일 내용을 바로 볼 수 있게 하는게 좋다고 생각해
    - pdf 파일 보기를 누르면 옆 화면에
    - 파일이 표시되도록 수정했습니다.

--- src/pages/detail.js
+++ src/pages/detail.js
@@ -1,77 +1,116 @@
 import Layout from "@/components/Layout";
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 
 export default function Detail() {
-  const fileInputRef = useRef(null);
+  const [files, setFiles] = useState({
+    resume: null,
+    coverLetter: null,
+    portfolio: null
+  });
+  const fileInputRefs = {
+    resume: useRef(null),
+    coverLetter: useRef(null),
+    portfolio: useRef(null)
+  };
+  const [activeFile, setActiveFile] = useState(null);
 
-  const handleFileUpload = (e) => {
+  const handleFileUpload = (e, fileType) => {
     const file = e.target.files[0];
-    // 선택 파일 업로드/추가하는 로직 삽입 필요
-    console.log('Uploaded file:', file);
+    if (file) {
+      setFiles(prevFiles => ({ ...prevFiles, [fileType]: file }));
+    }
+  };
+
+  const openFileExplorer = (fileType) => {
+    fileInputRefs[fileType].current.click();
+  };
+
+  const handleFileView = (fileType) => {
+    setActiveFile(fileType);
+  };
+
+  const renderFileContent = () => {
+    if (activeFile && files[activeFile]) {
+      return <embed src={URL.createObjectURL(files[activeFile])} type="application/pdf" width="100%" height="100%" />;
+    }
+    return <div>Please select a file.</div>;
   };
 
   return (
     <Layout>
       <div>
-        <input
-          type="file"
-          ref={fileInputRef}
-          style={{ display: 'none' }}
-          onChange={handleFileUpload}
-        />
         <div class="inset-0 flex flex-col justify-end">
-          <div class="bg-primary p-0 rounded-full w-8 h-8 text-center">
-            <p class="text-white font-bold place-self-center pt-1">X</p>
-          </div>
-
-          <div class="place-self-center bg-white p-4 rounded-2xl shadow drop-shadow-md mb-9 w-3/4 h-52">
+            <div class="bg-primary p-0 rounded-full w-8 h-8 text-center">
+              <p class="text-white font-bold place-self-center pt-1">X</p>
             </div>
-          
-          {/* 여기부터 pdf 파일 삽입 구간. */}
 
-          <div class="bg-secondary/80 p-3 py-5 place-self-center rounded-2xl shadow drop-shadow-lg w-3/4">
-            <div className="flex flex-col">
-
-              <div className="bg-white p-4 rounded-2xl">
-                <div className="flex flex-col mb-1">
-                  <span className="font-normal ml-2">이력서</span>
-                  <button
-                    className="bg-secondary/20 hover:bg-secondary/50 text-secondary py-2 px-4 rounded-2xl mt-2 "
-                    onClick={() => fileInputRef.current.click()}
-                  >
-                    PDF 파일 선택
-                  </button>
-                </div>
+            <div class="place-self-center bg-white p-4 rounded-2xl shadow drop-shadow-md mt-8 mb-9 w-full h-52">
+            </div>
+        </div>
+        
+        {/* 여기부터 pdf 파일 삽입 및 확인 구간 */}
+        <div className="flex">
+          <div className="w-1/4">
+            <div className="bg-white p-4 rounded-2xl mb-3">
+              <div className="flex flex-col mb-1">
+                <span className="font-normal ml-2">이력서</span>
+                <button
+                  className="bg-secondary/20 hover:bg-secondary/50 text-secondary py-2 px-4 rounded-2xl mt-2"
+                  onClick={() => handleFileView('resume')}
+                >
+                  {files.resume ? '파일 보기' : 'PDF 파일 선택'}
+                </button>
+                <input
+                  type="file"
+                  ref={fileInputRefs.resume}
+                  style={{ display: 'none' }}
+                  onChange={(e) => handleFileUpload(e, 'resume')}
+                />
               </div>
-
-              <div className="bg-white p-4 rounded-2xl mt-3">
-                <div className="flex flex-col mb-1">
-                  <span className="font-normal ml-2">자소서</span>
-                  <button
-                    className="bg-secondary/20 hover:bg-secondary/50 text-secondary py-2 px-4 rounded-2xl mt-2 "
-                    onClick={() => fileInputRef.current.click()}
-                  >
-                    PDF 파일 선택
-                  </button>
-                </div>
+            </div>
+            <div className="bg-white p-4 rounded-2xl mb-3">
+              <div className="flex flex-col mb-1">
+                <span className="font-normal ml-2">자소서</span>
+                <button
+                  className="bg-secondary/20 hover:bg-secondary/50 text-secondary py-2 px-4 rounded-2xl mt-2"
+                  onClick={() => handleFileView('coverLetter')}
+                >
+                  {files.coverLetter ? '파일 보기' : 'PDF 파일 선택'}
+                </button>
+                <input
+                  type="file"
+                  ref={fileInputRefs.coverLetter}
+                  style={{ display: 'none' }}
+                  onChange={(e) => handleFileUpload(e, 'coverLetter')}
+                />
               </div>
-
-              <div className="bg-white p-4 rounded-2xl mt-3 ">
-                <div className="flex flex-col mb-1">
-                  <span className="font-normal ml-2">포트폴리오</span>
-                  <button
-                    className="bg-secondary/20 hover:bg-secondary/50 text-secondary py-2 px-4 rounded-2xl mt-2 "
-                    onClick={() => fileInputRef.current.click()}
-                  >
-                    PDF 파일 선택
-                  </button>
-                </div>
+            </div>
+            <div className="bg-white p-4 rounded-2xl mb-3">
+              <div className="flex flex-col mb-1">
+                <span className="font-normal ml-2">포트폴리오</span>
+                <button
+                  className="bg-secondary/20 hover:bg-secondary/50 text-secondary py-2 px-4 rounded-2xl mt-2"
+                  onClick={() => handleFileView('portfolio')}
+                >
+                  {files.portfolio ? '파일 보기' : 'PDF 파일 선택'}
+                </button>
+                <input
+                  type="file"
+                  ref={fileInputRefs.portfolio}
+                  style={{ display: 'none' }}
+                  onChange={(e) => handleFileUpload(e, 'portfolio')}
+                />
               </div>
-
+            </div>
           </div>
-        </div>    
+          <div className="w-3/4">
+            <div className="bg-white p-4 rounded-2xl h-full">
+              {renderFileContent()}
+            </div>
+          </div>
         </div>
       </div>
     </Layout>
   );
 }
+
