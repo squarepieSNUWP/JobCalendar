9f323631be164e13c9781f7531ef48352f2bb359
styu12 <styu12@naver.com>
2023-06-04 11:26:25 +0900

    Init: 회고 기능 링크/데이터 세팅 - 헤더에 /review로 가는 링크 추가 - 더미 데이터 reviews 배열 가지게끔 수정

--- src/data/index.js
+++ src/data/index.js
@@ -1,5 +1,6 @@
 export const jobs = [
     {
+        id: 1,
         title: '프론트엔드 개발자 (3년 이상)',
         summary:`• 프론트 개발 경력 3년 이상 이신 분
         • Vue.js, Javascript(ES6+) 개발에 능숙하신 분
@@ -9,8 +10,21 @@ export const jobs = [
         occupation: "프론트엔드 개발자",
         startDate: "2023-05-01T00:00",
         endDate: "2023-05-29T24:00",
+        reviews: [
+            {
+                question: "협업하면서 가장 힘들었던 기억은?",
+                answer: "깃헙 사용이 어려웠어요.",
+            }, {
+                question: "가장 자신있는 프로그래밍 언어는?",
+                answer: "자바스크립트랑 파이썬이요.",
+            }, {
+                question: "실제 프로젝트를 배포해본 경험이 있나요?",
+                answer: "취뽀달력 만들면서 배포해봤어요.",
+            }
+        ]
     },
     {
+        id: 2,
         title: '백엔드 개발자 (3년 이상)',
         summary:`• 백 개발 경력 3년 이상 이신 분
         • Java, Spring Boot 개발에 능숙하신 분
@@ -20,6 +34,12 @@ export const jobs = [
         occupation: "백엔드 개발자",
         startDate: "2023-05-01T00:00",
         endDate: "2023-05-29T24:00",
+        reviews: [
+            {
+                question: "왜 백엔드에 지원했나요?",
+                answer: "UI를 만드는 작업보다 데이터를 다루는 작업이 더 재밌어서요.",
+            }
+        ]
     },
 
 ]
