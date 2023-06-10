a34a82843b5e0713e0518063142e52d8d5a25416
styu12 <styu12@naver.com>
2023-06-10 17:20:55 +0900

    Feat: reviewAPI & cvAPI - review CRU - cv CRU

--- src/api/review.js
+++ src/api/review.js
@@ -1,13 +1,48 @@
-export const getReviews = () => {
+import { db } from "@/firebase"
+import { addDoc, collection, doc, getDocs, orderBy, query, updateDoc, where } from "firebase/firestore"
 
+const reviewCollection = collection(db, "reviews");
+
+export const getReviews = async ( jobId ) => {
+    const q = query(
+        reviewCollection, 
+        where("jobId", "==", jobId),
+        orderBy("createdAt", "desc")
+    );
+
+    const results = await getDocs(q);
+
+    const res = results.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data()
+    }));
+
+    return res;
 }
 
-export const createReview = () => {
+export const createReview = async ( data ) => {
+    const { question, answer, jobId } = data;
 
+    const createdAt = new Date().toLocaleDateString();
+
+    const docRef = await addDoc(reviewCollection, {
+        question,
+        answer,
+        jobId,
+        createdAt
+      });
+
+    return docRef;
 }
 
-export const updateReview = () => {
+export const updateReview = async (data) => {
+    const reviewDoc = doc(reviewCollection, data.id);
+    const res = await updateDoc(reviewDoc, {
+        question: data.question,
+        answer: data.answer,
+    });
 
+    console.log(res);
 }
 
 export const deleteReview = () => {
