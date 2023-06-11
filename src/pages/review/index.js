67f9de794d36d54f03e487eec08d2e4e8f84f821
sunjeong512 <select@snu.ac.kr>
2023-06-11 22:19:23 +0900

    Design: 전체 레이아웃 고정
    - 메인 화면이 되는 container의 속성에 overflow 를 추가해
    - 내용 오버시 스크롤이 등장하도록 수정했습니다.

--- src/pages/review/index.js
+++ src/pages/review/index.js
@@ -33,8 +33,8 @@ export default function ReviewList() {
                         <path stroke="none" d="M0 0h24v24H0z"/>
                         <polyline points="9 6 15 12 9 18" />
                     </svg>
-                    <h2 className="text-xl font-bold tracking-tight text-gray-800/90 mt-3 mb-0.5">{j.title}</h2>
-                    <h3 className="text-lg font-normal text-gray-800/70">{j.company}</h3>
+                    <h2 className="text-xl font-bold tracking-tight text-gray-800/90 mt-3 mb-0.5">{j.company}</h2>
+                    <h3 className="text-lg font-normal text-gray-800/70">{j.title}</h3>
                 </div>
             ))}
         </Layout>
